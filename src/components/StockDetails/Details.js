b7a3225864d03e56d75c087ad9e6eefa514e3035
arifvtti <arif.vtti@gmail.com>
2022-06-01 09:55:31 +0600

    display limited data depend on current page and size

--- src/components/StockDetails/Details.js
+++ src/components/StockDetails/Details.js
@@ -3,15 +3,21 @@ import Detail from './Detail';
 
 const Details = () => {
     const [stocks, setStocks] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
     useEffect(() => {
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false")
             .then(res => res.json())
             .then(data => {
                 const totalPage = Math.ceil(data.length / 5);
-                const currentPage = 1;
-                setStocks(data.slice(currentPage * 5, (5 * (currentPage + 1))));
+                if (totalPage > currentPage) {
+                    setStocks(data.slice(currentPage * 5, (5 * (currentPage + 1))));
+                }
+                else {
+                    setCurrentPage(0);
+                }
             })
-    }, [])
+    }, [currentPage])
+
     return (
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg max-w-6xl mx-auto my-10 border">
             <div class="p-4 flex justify-start items-center">
@@ -21,7 +27,7 @@ const Details = () => {
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                     </div>
-                    <input type="text" id="table-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by company name" />
+                    <input type="text" id="table-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by name" />
                 </div>
             </div>
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -52,6 +58,11 @@ const Details = () => {
                         />)
                     }
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Footer</td>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     );
